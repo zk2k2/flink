d3902c4906a7286a364df599584de6c60a22dc4a
zayneb-1661 <zayneb.fathalli@insat.ucar.tn>
2025-01-26 15:31:17 +0100

    Zayneb & Louay : fixed the user location with appropriate ORM type & added a user hobbies service to correct crud operations on user's signup as well as registering the location correctly in the DB

--- flink-backend/src/modules/user/user.module.ts
+++ flink-backend/src/modules/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/modules/user/entities/user.entity';
 import { Location } from '../../common/entities/location.entity';
+import { HobbyModule } from '../hobby/hobby.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Location])],
+  imports: [HobbyModule, TypeOrmModule.forFeature([User, Location])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
--- flink-backend/src/modules/user/user.service.ts
+++ flink-backend/src/modules/user/user.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import * as bcrypt from 'bcrypt'; // Import bcrypt for password hashing and validation
-
+import * as bcrypt from 'bcrypt'; 
 import { CommonService } from 'src/common/service/common.service';
 import { User } from 'src/modules/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { SignupDto } from './dto/signup-user.dto';
 import { UpdateProfileDto } from './dto/update-user.dto';
 import { Location } from '../../common/entities/location.entity';
-
+import { HobbyService } from '../hobby/hobby.service';
+import { UserHobbiesService } from '../hobby/user-hobbies.service';
+import { UserHobby } from '../hobby/entities/user-hobby.entity';
 @Injectable()
 export class UserService extends CommonService<
   User,
@@ -17,13 +18,14 @@ export class UserService extends CommonService<
 > {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
-    @InjectRepository(Location)
-    private readonly locationRepository: Repository<Location>,
+    @InjectRepository(Location) private readonly locationRepository: Repository<Location>,
+    private readonly hobbyService: HobbyService,
+    private readonly UserHobbiesService: UserHobbiesService,
   ) {
     super(userRepository);
   }
-
   async create(signupDto: SignupDto): Promise<User> {
+  
     const userExists = await this.userRepository.findOne({
       where: [
         { email: signupDto.email },
@@ -31,25 +33,45 @@ export class UserService extends CommonService<
         { phone: signupDto.phone },
       ],
     });
-
+  
     if (userExists) {
-      throw new Error(
-        'User with provided email, username, or phone already exists',
-      );
+      throw new Error('User already exists');
     }
-
-    // Ensure location is correctly handled
-    const newLocation = this.locationRepository.create(signupDto.location);
+    const newLocation = this.locationRepository.create({
+      name: signupDto.location.name,
+      coordinates: {
+        type: 'Point',
+        coordinates: [signupDto.location.longitude, signupDto.location.latitude],
+      }, 
+    });
     const savedLocation = await this.locationRepository.save(newLocation);
 
+  
     const newUser = this.userRepository.create({
       ...signupDto,
       location: savedLocation,
     });
+    const savedUser = await this.userRepository.save(newUser);
 
-    return await this.userRepository.save(newUser);
+    if (signupDto.hobbies && signupDto.hobbies.length > 0) {
+      for (const hobbyDto of signupDto.hobbies) {
+        const hobby = await this.hobbyService.findByField('title',hobbyDto.title);
+        if (!hobby) {
+          throw new Error(`Hobby "${hobbyDto.title}" does not exist.`);
+        }
+        const newUserHobby = new UserHobby();
+        newUserHobby.user = savedUser;
+        newUserHobby.hobby = hobby;
+        newUserHobby.interestLevel = hobbyDto.interestLevel;
+        const savedUserHobby  = await this.UserHobbiesService.create(
+          newUserHobby
+        );
+      }
+
+    return savedUser;
   }
-
+}
+  
   async findByField(field: string, value: string): Promise<User> {
     return this.userRepository.findOne({ where: { [field]: value } });
   }
