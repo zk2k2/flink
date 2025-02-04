46a176c3936794a50af0da7a1fa273a56465ecc3
zk2k2 <ziedkharrat4juin@gmail.com>
2025-02-04 19:25:49 +0100

    added interceptor for auth

--- flink-frontend/src/app/app.module.ts
+++ flink-frontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeedModule } from './features/feed/feed.module';
 import { LandingModule } from './features/landing/landing.module';
 import { ActivityModule } from './features/activity/activity.module';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,8 +23,11 @@ import { ActivityModule } from './features/activity/activity.module';
     FeedModule,
     LandingModule,
     ActivityModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- flink-frontend/src/app/core/services/auth.service.ts
+++ flink-frontend/src/app/core/services/auth.service.ts
@@ -4,12 +4,6 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private isAuthenticated = false;
 
-  login(email: string, password: string): void {
-    // Call your backend API here
-    this.isAuthenticated = true;
-    localStorage.setItem('token', 'dummy-token');
-  }
-
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
   }
