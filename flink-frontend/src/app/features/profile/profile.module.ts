bec4c51e030e520fce4b9591f43f46c71f6763ab
nidhal-jabnouni <nidhal.jabnouni@insat.ucar.tn>
2025-02-05 20:27:52 +0100

    added map use and file upload to profile edit

--- flink-frontend/src/app/features/profile/profile.module.ts
+++ flink-frontend/src/app/features/profile/profile.module.ts
@@ -6,6 +6,7 @@ import { ProfileComponent } from './profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { UpdateUserInformationComponent } from './update-user-information/update-user-information.component';
 import { UserProfileFormComponent } from './user-profile-form/user-profile-form.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { UserProfileFormComponent } from './user-profile-form/user-profile-form.
     ProfileRoutingModule,
     ReactiveFormsModule,
     SharedModule,
+    MatSnackBarModule, 
   ],
 })
 export class ProfileModule {}
