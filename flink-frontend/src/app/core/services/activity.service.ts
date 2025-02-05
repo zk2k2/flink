4c71aee995e44ec8cf9f7cb0f469f888d52a945f
zk2k2 <ziedkharrat4juin@gmail.com>
2025-02-05 13:10:28 +0100

    fixed a minor import bug

--- flink-frontend/src/app/core/services/activity.service.ts
+++ flink-frontend/src/app/core/services/activity.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { ActivityCard } from '../../shared/types/ActivityCard';
